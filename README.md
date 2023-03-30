5bf3d32fdcf3ce173fc01ff88c0e6070adfecc09
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-03-30 21:37:28 +0100

    Completed code and added comments

--- README.md
+++ README.md
@@ -1,53 +1,43 @@
-# asdsa
-![a](https://img.shields.io/badge/LICENSE-a-red)
-  
+# README Generator
 
 ## Description
 
-undefined
+I have created a back end application that allows you to use questions in your terminal to created a README file.
 
 ## Table of Contents
 
-- [Technologies Used](#technologies-used)
-- [Challenges Faced](#challenges-faced)
-- [Successes Achieved](#successes-achieved)
-- [Demo](#demo)
-- [Deployed Site](#deployed-site)
-- [Repository](#repository)
-- [Credits](#credits)
+- [Title](#title)
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Contributing](#contributing)
+- [Questions](#questions)
 - [License](#license)
 
-## Technologies Used
-
-undefined
-
 ## Installation
-undefined
+You need to have VS Code, Node and GitBash(windows)
 
-## Challenges Faced
+## Usage
 
-## License 
+Once you have the neccessary technologies installed you will need to clone down the repository so it is within your local files. Then you will need open the directory in VS code as well as the already exisitng terminal. Youw ill then need to run the command 'node index.js'. Questions will then begin to appear in your terminal. When you have answered all of the questions you will see a README file appear within your directory. You can then use this to create all of your README files.
+
+## Tests
+
+You can test my application by firstly running the command 'node index.js' as this should prompt the questions. You can also enter a link where prompted and ensure this then creates as a functional link as well as ensuring a badge appears when a license is selected or that the section isn't created if the option 'none' is selected. 
+
+## Contributing
+
+Faith Meades
+
+## Questions
+Username: FaithsCoding
+URL: https://github.com/FaithsCoding
+You can send me any questions via email: faithscoding@outlook.com
     
-  ashdfkjsadfkjshfsdhf sdfjhasdf sakdjh ashkjsa dfh a.
+## License
 
-    
+This project is licensed under the MIT license.
 
-## Successes Achieved
+![MIT](https://img.shields.io/badge/LICENSE-MIT-red)
 
-undefined
-
-## Demo
-
-![Demo](undefined)
-
-## Deployed Site
-
-![Deployed]undefined
-
-## Repository
-
-![Repository]undefined
-
-## Credits
-
-This project was created by undefined.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,42 +1,6 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license === "none") {
-    return "";
-  } else {
-    return `![${license}](https://img.shields.io/badge/LICENSE-${license}-red)`;
-  }
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license === "none") {
-    return "";
-  } else {
-    return `- [License](#license)`;
-  }
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license === "none") {
-    return "";
-  } else {
-    return `## License 
-    
-  ashdfkjsadfkjshfsdhf sdfjhasdf sakdjh ashkjsa dfh ${license}.
-
-    `;
-  }
-}
-
-// TODO: Create a function to generate markdown for README
+//This is the function to create the Markdwon
 function generateMarkdown(data) {
-  return `# ${data.title}
-${renderLicenseBadge(data.license)}
-  
+  let markdown = `# ${data.title}
 
 ## Description
 
@@ -44,46 +8,91 @@ ${data.description}
 
 ## Table of Contents
 
-- [Technologies Used](#technologies-used)
-- [Challenges Faced](#challenges-faced)
-- [Successes Achieved](#successes-achieved)
-- [Demo](#demo)
-- [Deployed Site](#deployed-site)
-- [Repository](#repository)
-- [Credits](#credits)
-${renderLicenseLink(data.license)}
+- [Title](#title)
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Contributing](#contributing)
+- [Questions](#questions)`;
+//The table of contents is navgational to the relevant sections
 
-## Technologies Used
+  // This is an if statement making sure a section IS NOT created if none is selected
+  if (data.selectedLicense !== "none") {
+    markdown += `
+- [License](#license)`;
+  }
 
-${data.technologies}
+  markdown += `
 
 ## Installation
 ${data.installation}
 
-## Challenges Faced
+## Usage
 
-${renderLicenseSection (data.license)}
+${data.usage}
 
-## Successes Achieved
+## Tests
 
-${data.successes}
+${data.tests}
 
-## Demo
+## Contributing
 
-![Demo](${data.demonstration})
+${data.contributing}
 
-## Deployed Site
+## Questions
+Username: ${data.gitUsername},
+URL: ${data.gitUrl},
+You can send me any questions via email: ${data.questions}`;
 
-![Deployed]${data.deployed}
+  // This creates the license section, link and badge
+  if (data.selectedLicense !== "none") {
+    markdown += `
+   
+## License
 
-## Repository
+This project is licensed under the ${data.selectedLicense} license.
 
-![Repository]${data.repository}
+${renderLicenseBadge(data.selectedLicense)}
 
-## Credits
-
-This project was created by ${data.creator}.
 `;
+  }
+
+  return markdown;
+}
+
+// This function returns a license badge based on which license is passed in
+// If there is no license, this returns an empty string
+function renderLicenseBadge(selectedLicense) {
+  if (selectedLicense === "none") {
+    return "";
+  } else {
+    return `![${selectedLicense}](https://img.shields.io/badge/LICENSE-${selectedLicense}-red)`;
+  }
+}
+
+// This function returns the license link
+// If there is no license, this returns an empty string
+function renderLicenseLink(selectedLicense) {
+  if (selectedLicense === "none") {
+    return "";
+  } else {
+    return `- [License](#license)`;
+  }
+}
+
+// This function returns the license section of README
+// If there is no license, this return an empty string
+function renderLicenseSection(selectedLicense) {
+  if (selectedLicense === "none") {
+    return "";
+  } else {
+    return `## License 
+    
+This project is licensed under the ${selectedLicense} license.
+    
+    `;
+  }
 }
 
 module.exports = generateMarkdown;
